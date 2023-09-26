e711a282cdd71d622561eb6c264ddd386c17229d
Ryan Dugan <70173704+RyanDugan@users.noreply.github.com>
2023-09-26 13:59:29 -0500

    added css

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 console.log("Hello NVC");
+
+const animals = ["Fish", "Cat", "Dog", "Bird"];
+
+for (name of animals) {
+  console.log(`Hello there ${name}`);
+  if (name === "Dog") {
+    console.log("We found Arf!");
+    break;
+  }
+}
+
+let loading = 0;
+
+while (loading < 100) {
+  console.log("Website is still loading...");
+
+  loading += 2;
+}
