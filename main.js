e148c89ad9508fdce3e1cb338e525b9ca4036b77
Ryan Dugan <70173704+RyanDugan@users.noreply.github.com>
2023-09-27 11:59:43 -0500

    added more test js

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ console.log("Hello NVC");
 
 const animals = ["Fish", "Cat", "Dog", "Bird"];
 
-for (name of animals) {
-  console.log(`Hello there ${name}`);
-  if (name === "Dog") {
+for (test of animals) {
+  console.log(`Hello there ${test}`);
+  if (test === "Dog") {
     console.log("We found Arf!");
     break;
   }
@@ -16,4 +16,15 @@ while (loading < 100) {
   console.log("Website is still loading...");
 
   loading += 2;
+}
+
+let x;
+for (x = 0; x < 10; x++) {
+  console.log(x);
+}
+
+const fruits = ["Apple", "Banana", "Orange"];
+
+for (x of fruits) {
+  console.log(x);
 }
